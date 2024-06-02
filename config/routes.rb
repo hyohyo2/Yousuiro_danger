1b4db3b3706dcd0247e8cb28668f7c688fd39967
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-02 10:10:49 +0000

    [Add]image_professing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
