b77ade98dba1947f9785aac5631b09dc3b1f4dd9
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-02 13:07:55 +0000

    [Add]homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,30 @@
 Rails.application.routes.draw do
-  namespace :public do
+  namespace :admin do
     get 'homes/top'
-    get 'homes/about'
   end
+  namespace :public do
+      get 'homes/about'
+  end
+  # ユーザ用
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
   
+  # 管理者用
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  
+  # ユーザ用
+  root to: "public/homes#top"
+  get '/about' => 'public/homes#about'
+  
+  # 管理者用
+  namespace :admin do
+    root to: 'homes#top'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
