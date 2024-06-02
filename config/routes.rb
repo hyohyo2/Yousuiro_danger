06221acc378c10303a1f03f213de803747470637
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-02 09:38:39 +0000

    [Fix]devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sesions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
