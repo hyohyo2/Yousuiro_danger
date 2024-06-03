3c9017dd1fa2df5cc32a19e7c08a7814772ecaa4
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-03 08:38:15 +0000

    [Fix]login,signin layout

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Yousuiro
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
