5ee20570d7bee0a5ca6183e5cfdd35595dfe9379
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-04 03:06:46 +0000

    [Add] enum導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ApplicationRecord
   
   belongs_to :user
   
+  enum status: { danger: 0, safety: 1 }
+  
+  validates :post_code, presence: true
+  validates :prefecture_address, presence: true
+  validates :city_address, presence: true
+  validates :detail, presence: true
+  validates :status, presence: true
+  
   has_one_attached :image
   
 end
