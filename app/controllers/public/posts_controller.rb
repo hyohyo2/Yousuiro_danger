619d4731ad68984aac3e2386957bb5ef8b8ff05a
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-04 06:54:06 +0000

    新規投稿ページ作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -4,10 +4,14 @@ class Public::PostsController < ApplicationController
   end
   
   def create
-    
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post.id)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -23,4 +27,11 @@ class Public::PostsController < ApplicationController
 
   def timeline
   end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:image, :post_code, :prefecture_address, :city_address, :block_address, :detail, :status)
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,9 @@ class Post < ApplicationRecord
   
   has_one_attached :image
   
+  
+  def get_image(width, height)
+    image.variant(resize: "#{width}x#{height}!").processed
+  end
+  
 end
