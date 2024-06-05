78657836070724f7ba4750b3cf336cd753e79bad
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-05 00:35:04 +0000

    [Add] ユーザ・投稿seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザ情報
+User.create!(
+  email: 'test@test.com',
+  password: 'example',
+  name: 'テスト太郎',
+  introduction: 'よろしくね',
+  is_active: true
+)
+User.create!(
+  email: 'test1@test.com',
+  password: 'example',
+  name: 'テスト花子',
+  introduction: 'よろしくね',
+  is_active: true
+)
+
+# 投稿情報
+Post.create!(
+  user_id: User.find_by(name: 'テスト太郎').id,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/No_image.jpg")),filename: 'no_image.jpg'),
+  post_code: '1234567',
+  prefecture_address: '岡山県',
+  city_address: '岡山市北区',
+  block_address: '駅元町１－１',
+  detail: 'ここは柵がなくて危険です！近くを通るときはお気を付けください。',
+  status: 0
+)
+
+Post.create!(
+  user_id: User.find_by(name: 'テスト花子').id,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/No_image.jpg")),filename: 'no_image.jpg'),
+  post_code: '1234567',
+  prefecture_address: '岡山県',
+  city_address: '岡山市北区',
+  block_address: '駅元町１－２',
+  detail: 'ここは柵がなく危険でしたが、昨日柵が設置されました！',
+  status: 1
+)
