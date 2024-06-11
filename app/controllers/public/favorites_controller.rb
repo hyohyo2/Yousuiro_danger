4399ed4657046300d5a34607426fd86676bee42f
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-11 10:00:48 +0000

    詳細ページボタン追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,10 +1,16 @@
 class Public::FavoritesController < ApplicationController
   def create
-    
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to post_path(post.id)
   end
   
   def destroy
-    
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite.destroy
+    redirect_to post_path(post.id)
   end
   
 end
