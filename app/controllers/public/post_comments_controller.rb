94af0862d4b7ffdaa6d5d23c547f960c201401a4
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-12 13:48:13 +0000

    [Add]コメント機能非同期通信

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,28 +2,15 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    if comment.save
-      flash[:notice] = "コメントを投稿しました。"
-      redirect_to request.referer
-    else
-      flash[:alert] = "コメント投稿に失敗しました。"
-      redirect_to request.referer
-    end
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
 
   def destroy
-    comment = PostComment.find(params[:id])
-    if comment.destroy
-     flash[:notice] = "コメントを削除しました。"
-      redirect_to request.referer
-    else
-      flash[:alert] = "コメント削除に失敗しました。"
-      redirect_to request.referer
-    end
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
   end
-
   private
 
   def post_comment_params
