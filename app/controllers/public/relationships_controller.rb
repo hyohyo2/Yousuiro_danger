b4b675ae8248a46b9037673ce4fe22db36f026b3
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-12 07:07:48 +0000

    [Add]controller

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,14 @@
+class Public::RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+  end
+  
+  def destroy
+  end
+  
+  def followings
+  end
+  
+  def followers
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_11_092207) do
+ActiveRecord::Schema.define(version: 2024_06_12_064705) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 2024_06_11_092207) do
     t.string "block_address"
     t.text "detail", null: false
     t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
