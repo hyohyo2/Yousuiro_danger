0f5c692844f77ea2b0dd99990e387f3e2c55f2ae
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-12 02:48:47 +0000

    [Add]非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,14 @@
 class Public::FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_to request.referer
   end
   
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to request.referer
   end
   
 end
