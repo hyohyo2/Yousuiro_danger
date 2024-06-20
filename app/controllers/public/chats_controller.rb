4dd1c21671c77aa22565ee07ece753568feec3c0
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-20 11:03:46 +0000

    [Add]DM_list

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -4,22 +4,30 @@ class Public::ChatsController < ApplicationController
   before_action :ensure_guest_user
 
   def show
+    # チャット相手を取得
     @user = User.find(params[:id])
+    # ログインユーザーが参加しているチャットルームの一覧を取得
     rooms = current_user.user_rooms.pluck(:room_id)
+    # チャットルームの存在の確認
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
-
+    
     unless user_rooms.nil?
+      # チャットルームがあればチャットルームを表示
       @room = user_rooms.room
     else
+      # なければ新しく作成する
       @room = Room.new
       @room.save
-
+      
+      # チャットルームにログインユーザーを追加
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
+      # 相手ユーザーを追加
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
-
+    
+    # チャットのメッセージを取得
     @chats = @room.chats
-
+    # 新しくメッセージを作成
     @chat = Chat.new(room_id: @room.id)
 
   end
@@ -51,7 +59,7 @@ class Public::ChatsController < ApplicationController
   end
   def ensure_guest_user
     if current_user.guest_user
-      redirect_to user_path(current_user.id)
+      redirect_to user_path(current_user.id), alert: "ゲストユーザーはDM機能を利用できません"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
       resource :favorite, only:[:create, :destroy]
     end
     resources :chats, only:[:show, :create, :destroy]
+    resources :rooms, only:[:index]
     resources :notifications, only:[:index, :destroy, :update]
     resource :map, only:[:show]
   end
