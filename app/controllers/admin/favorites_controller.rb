aa631a6ca5ea3f87ec31b58c5c0ad1e2b020afe0
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-20 02:29:37 +0000

    [Fix]layout

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -2,6 +2,6 @@ class Admin::FavoritesController < ApplicationController
   def favorites_post
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
-    @favorite_posts = Post.where(id: favorites).page(params[:page]).per(3).order('id DESC')
+    @favorite_posts = Post.where(id: favorites).page(params[:page]).per(20).order('id DESC')
   end
 end
