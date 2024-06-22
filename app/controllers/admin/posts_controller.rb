b58a14f8506cf9b0e52bee1688249e1db1fe29b9
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-22 03:14:54 +0000

    [Fix]ページ遷移先

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,12 +3,12 @@ class Admin::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     if @post.destroy
       flash[:notice] = "投稿を削除しました。"
-      redirect_to admin_root_path
+      redirect_to admin_user_path(@post.user)
     else
       flash.now[:alert] = "投稿内容の削除に失敗しました。"
       render :show
