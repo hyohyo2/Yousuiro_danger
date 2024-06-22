1fe42a3bb12cab60c5f1be7da7657ce9a60a22be
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-22 04:25:05 +0000

    [Fix]ルート確認

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -1,7 +1,3 @@
 class Admin::FavoritesController < ApplicationController
-  def favorites_post
-    @user = User.find(params[:id])
-    favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
-    @favorite_posts = Post.where(id: favorites).page(params[:page]).per(20).order('id DESC')
-  end
+  
 end
