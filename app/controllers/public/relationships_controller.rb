a7c3ad2e1a617c8a4f2b17a2bce691f39bd6abcd
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-24 09:16:38 +0000

    [Fix]開発環境テスト

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,26 +1,37 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only:[:followers]
   # フォローする
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
   end
-  
+
   # フォローを解除
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
   end
-  
+
   # フォローユーザー一覧
   def followings
     @user = User.find(params[:user_id])
     @users = @user.followings.page(params[:page]).per(10).order('users.id DESC')
   end
-  
+
   # フォロワーユーザー一覧
   def followers
     @user = User.find(params[:user_id])
     @users = @user.followers.page(params[:page]).per(10).order('users.id DESC')
   end
+
+  private
+
+  # ゲストログイン時のアクセス制限
+  def ensure_guest_user
+    if current_user.guest_user
+      redirect_to user_path(current_user.id), alert: "ゲストユーザーはご利用できません。"
+    end
+  end
+
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifiable, polymorphic: true
 
-  # DM受信時の
+  # DM受信時の通知
   validates :user_id, presence: true
   validates :notifiable_type, presence: true
   validates :notifiable_id, presence: true
