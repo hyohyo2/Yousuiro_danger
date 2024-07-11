f2898c638adac783fec73bb4f72f5938eed7412b
hyohyo2 <hono.handball.0910@gmail.com>
2024-07-11 10:55:29 +0000

    [Fix]readme

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,12 +18,13 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-
+  # 管理者ログイン後の遷移先
   def admin_sign_in_path_for(resource)
     admin_root_path
   end
 
-  def after_sign_out_path_for(resource) #管理者のログアウト後の遷移先
+  #管理者ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
     new_admin_session_path
   end
 
