b7e3ac662ac10af8dc1cdc398e0d076a9bb25636
hyohyo2 <hono.handball.0910@gmail.com>
2024-07-28 04:48:00 +0000

    [Fix]レビュー後修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/default-image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize: "#{width}x#{height}!").processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   # 指定ユーザーのフォロー
